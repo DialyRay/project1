0af7953b4ac770cc4a63b9c29ad356cc7b6f967f
dialyRay <dialyray@gmail.com>
2025-01-08 17:55:49 +0300

    123

--- console.js
+++ console.js
@@ -1,3 +1,14 @@
-console.log(asdasdasdadasdasdasds);
-console.log("123");
-console.log("test");
+function test (x,y,z){ 
+    let arr = [true, false, true, false, true, false, true, false, true, true];  
+    let bal = x; let dep = y; 
+    for(let i = 0; i < z; i++){
+        let a = Math.floor(Math.random()*arr.length +1);
+        let result = arr[a]; 
+        if(result == true){
+            bal = bal + dep*2 
+        } else  {
+             bal = bal - dep} 
+    } 
+    console.log(bal) 
+}
+console.log(test(20,2,20));

6e8d6f4619052fcadb56b0cd8d18e34fd8fe7144
dialyRay <dialyray@gmail.com>
2025-01-08 18:31:58 +0300

    redakt

--- console.js
+++ console.js
@@ -1,14 +1,12 @@
-function test(x, y, z) {
+function test(bal, bet, attempts) {
     let arr = [true, false, true, false, true, false, true, false, true, true];
-    let bal = x;
-    let dep = y;
-    for (let i = 0; i < z; i++) {
+    for (let i = 0; i < attempts; i++) {
       let a = Math.floor(Math.random() * arr.length + 1);
       let result = arr[a];
       if (result == true) {
-        bal = bal + dep * 2;
+        bal = bal + bet * 2;
       } else {
-        bal = bal - dep
+        bal = bal - bet
       }
     }
     console.log(bal);
